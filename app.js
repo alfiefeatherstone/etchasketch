5fd90d2b911faaac265f13de15ec4f99f003b2bb
alfiefeatherstone <dan.r.williams@hotmail.co.uk>
2016-06-12 21:34:21 +0100

    first colour change finished. container populated properly

--- app.js
+++ app.js
@@ -3,22 +3,42 @@ var cont = $("#container");
 var newGrid = $("#new-grid");
 var suggest;
 var sty = $(".square-pane");
+var colourChange = $("#randColour");
 
+$("div").mouseover(function(){
+    $(event.target).css("background-color", "blue")
+});
 newGrid.on("click", function(){
     suggest = prompt("Enter a number between 1 - 64");
-       var wist = Math.floor(800 / suggest);
-    sty.css({"height":wist,"width": wist});
- 
-    popGrid(suggest*suggest);
+    var dub = suggest*suggest;
+    var wist = 800 / suggest;
+    console.log(wist);
+    popGrid(dub, wist);
 })
-while (i < 16) {
+while (i < 256) {
     cont.append("<div class='square-pane'></div>");
     i++;
 }
-function popGrid(as) {
+function popGrid(as, pf) {
     var t = 0;
+    cont.empty();
     while (t < as) {
-        cont.append("<div class='square-pane'></div>");
+        $("<div></div>", {
+            "class": "square-pane"
+        }).width(pf).height(pf).appendTo(cont);
         t++;
     }
 }
+function colour() {
+    return Math.floor((Math.random()*255));
+}
+
+function rgba(){
+    return "rgb(" + colour() + "," + colour() + "," + colour() + ");";
+}
+
+colourChange.on("click", function(){
+    $("div").mouseover(function(){
+        $(event.target).css("background-color", rgba())
+});
+})
