22f9525473569a33e5290dc741329beba6126b5b
alfiefeatherstone <dan.r.williams@hotmail.co.uk>
2016-06-15 11:32:00 +0100

    random colour button finished

--- app.js
+++ app.js
@@ -2,36 +2,15 @@ var i = 0;
 var cont = $("#container");
 var newGrid = $("#new-grid");
 var reset = $("#reset");
-var shading = $("#shading")
-
-newGrid.on("click", function(){
-    var suggest = prompt("Enter a number between 1 - 64");
-    var dub = suggest*suggest;
-    var wist = 800 / suggest;
-    popGrid(dub, wist)
-    $("div").hover(function(){
-        $(event.target).css({"background-color": "green"})
-});
-})
-reset.on("click", function(){
-    $("div").css("background-color", "white");
-})
-shading.on("click", function(){
-    $("div").hover(function(){
-        $(this).css("background-colour", "green")});
-});
-
-
-
-
+var shading = $("#shading");
+var rand = $("#colourChange");
 
 
 function colour() {
     return Math.floor((Math.random()*255));
 }
-
 function rgba(){
-    return "rgb(" + colour() + "," + colour() + "," + colour() + ");";
+    return "rgb(" + colour() + "," + colour() + "," + colour() + ")";
 }
 function popGrid(as, pf) {
     var t = 0;
@@ -42,6 +21,31 @@ function popGrid(as, pf) {
         t++;
     }
 }
+
+
+newGrid.on("click", function(){
+    var suggest = prompt("Enter a number between 1 - 64");
+    var dub = suggest*suggest;
+    var wist = 800 / suggest;
+    popGrid(dub, wist)
+    $("div").on("mouseenter mouseleave", function(){
+        $(event.target).css({"background-color": "blue"})
+});
+})
+reset.on("click", function(){
+    $("div").css("background-color", "rgb(255,255,255)");
+})
+rand.on("click", function(){
+    $("div").on("mouseenter mouseleave", function(){
+        var t = rgba();
+        $(event.target).css("background-color", t);
+    })
+})
+
+
+
+
+
 
 
 // colourChange.on("click", function(){
